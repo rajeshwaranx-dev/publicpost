98d03918a270e7695b8e6f942c4930cedb640daa
rajeshwaranx-dev <115334651+rajeshwaranx-dev@users.noreply.github.com>
2026-03-12 21:45:30 +0530

    Update poster_bot.py

--- posterbot/poster_bot.py
+++ posterbot/poster_bot.py
@@ -42,7 +42,7 @@ from telegram.ext import (
 from telegram.constants import ParseMode
 
 # ── Config ────────────────────────────────────────────────────
-BOT_TOKEN     = os.environ["BOT_TOKEN", "8647213611:AAH1Q6hmWVwnzzwPVJkfZMUOux2773_x1gs"]
+BOT_TOKEN     = os.environ.get("BOT_TOKEN", "8647213611:AAH1Q6hmWVwnzzwPVJkfZMUOux2773_x1gs")
 TMDB_API_KEY  = os.environ.get("TMDB_API_KEY", "992a90609f7400986165a20903ab9006")
 MONGO_URL     = os.environ.get("MONGO_URL", "mongodb+srv://bharath:bharathx@cluster0.7zcyu4q.mongodb.net/?appName=Cluster0")
 MONGO_DB_NAME = os.environ.get("MONGO_DB_NAME", "bharath")
