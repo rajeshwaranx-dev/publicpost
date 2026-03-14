0c9adc35126bd3e88aef9711bf2894b7143acc60
rajeshwaranx-dev <115334651+rajeshwaranx-dev@users.noreply.github.com>
2026-03-14 08:59:18 +0530

    Update config.py

--- posterbot/config.py
+++ posterbot/config.py
@@ -15,8 +15,8 @@ log = logging.getLogger(__name__)
 # ── Environment ───────────────────────────────────────────────
 BOT_TOKEN     = os.environ.get("BOT_TOKEN", "8647213611:AAH1Q6hmWVwnzzwPVJkfZMUOux2773_x1gs")
 TMDB_API_KEY  = os.environ.get("TMDB_API_KEY", "992a90609f7400986165a20903ab9006")
-MONGO_URL     = os.environ.get("MONGO_URL", "mongodb+srv://bharath:bharathx@cluster0.7zcyu4q.mongodb.net/?appName=Cluster0")
-MONGO_DB_NAME = os.environ.get("MONGO_DB_NAME", "bharath")
+MONGO_URL     = os.environ.get("MONGO_URL", "mongodb+srv://RajeshLcu2:Rajeshx@cluster0.0razpdy.mongodb.net/?appName=Cluster0")
+MONGO_DB_NAME = os.environ.get("MONGO_DB_NAME", "RajeshLcu2")
 ADMIN_IDS: set[int] = {
     int(x.strip()) for x in os.environ.get("ADMIN_IDS", "").split(",")
     if x.strip().isdigit()
